158203dcdbaeac162d8e07a4fcbd34477101e4fb
Camila Pinzon <camilapinz18@gmail.com>
2023-01-28 10:23:19 -0500

    Navigation between screens
    -html file unified
    -Navigation between screens added
    -launchError(message) method added to generate error messages in the front-end

--- main.js
+++ main.js
@@ -1,68 +1,154 @@
 const app = Vue.createApp({
   data () {
     return {
-      positions: [
+      activeLogin: true,
+      activeAdmin: false,
+      activeSeller: false,
+      activeSecretary: false,
+      activessembler: false,
+      isError: true,
+      messageError: '',
+      user: '',
+      password: '',
+      isChange: false,
+      btnChange: 'Change',
+      loader: false,
+      messageLoader: '',
+      positionSelect: null,
+      salary: null,
+      quantyShoes: null,
+      priceShoes: null,
+      commision: null,
+      users: [
         {
-          position: 'Administrator',
+          name: 'admin',
           password: 'adm1'
         },
         {
-          position: 'Secretary',
-          password: 'scr1',
-          salary: 0
-        },
-        {
-          position: 'Seller',
-          password: 'sel1',
-          salary: 0
-        },
-        {
-          position: 'Assembler',
+          name: 'assembler',
           password: 'asm1',
-          salary: 0
+          salary: 1300000,
+          quantyShoes: 500,
+          priceShoes: 25000
+        },
+        {
+          name: 'seller',
+          password: 'sel1',
+          salary: 1400000,
+          commision: 600
+        },
+        {
+          name: 'secretary',
+          password: 'srt1',
+          salary: 1500000
         }
-      ],
-      user: '',
-      password: '',
-      positionSelect: '',
-      salaryInput: 0
+      ]
+    }
+  },
+  watch: {
+    positionSelect (newValue, oldValue) {
+      this.isChange = false
+      this.changeBtnChangeName()
+      if (this.positionSelect) {
+        const user = this.users.find(element => element.name === newValue)
+        if (user) {
+          this.salary = user.salary
+          this.quantyShoes = null
+          this.priceShoes = null
+          this.commision = null
+          switch (user.name) {
+            case 'assembler':
+              this.quantyShoes = user.quantyShoes
+              this.priceShoes = user.priceShoes
+              break
+            case 'seller':
+              this.commision = user.commision
+              break
+          }
+        }
+      }
     }
   },
   methods: {
+    /*--GENERAL-- */
+    launchError (message) {
+      this.isError = true
+      this.messageError = message
+      setTimeout(() => {
+        this.isError = false
+        this.messageError = ''
+      }, 2000)
+    },
+    /*--LOGIN--*/
     checkData () {
-      const isUser = this.positions.find(
-        user => user.position === this.user.trim()
-      )
+      const isUser = this.users.find(user => user.name === this.user.trim())
       if (isUser) {
         if (isUser.password.trim() === this.password) {
-          alert('Successful login')
           ;(this.user = ''), (this.password = '')
+          this.activeLogin = false
+          this.activeAdmin = true
+          console.log('Successful login')
         } else {
-          alert('Wrong password')
+          this.launchError('Wrong password')
         }
       } else {
-        alert('The username is wrong or doesnt exists')
+        this.launchError('The username is wrong or doesnt exists')
       }
     },
     checkFields () {
       if (this.user === '' || this.password === '') {
-        alert('You must fill all the fields to continue')
+        this.launchError('You must fill all the fields to continue')
       } else {
         this.checkData()
       }
     },
-
+    /*--ADMIN--*/
     save () {
-      let toModify = this.positions.find(employee =>
-        employee.position === this.positionSelect.position
-          ? (employee.salary = this.salaryInput)
-          : ''
-      )
-      this.positions.map(pos => console.log('m', pos))
-      this.salaryInput = ''
+      this.messageLoader = 'Please wait...'
+      this.changeLoaderState()
+      setTimeout(() => {
+        const user = this.users.find(
+          element => element.name === this.positionSelect
+        )
+        if (user) {
+          user.salary = this.salary
+          switch (user.name) {
+            case 'assembler':
+              user.quantyShoes = this.quantyShoes
+              user.priceShoes = this.priceShoes
+              break
+            case 'seller':
+              user.commision = this.commision
+              break
+          }
+          this.users.splice(
+            this.users.findIndex(u => u.name === this.positionSelect),
+            1,
+            user
+          )
+        }
+        this.changeLoaderState()
+        this.changeBtnChangeName()
+        this.clearForm()
+      }, 10)
     },
     change () {
-      console.log('change')
+      this.isChange = !this.isChange
+      this.changeBtnChangeName()
+    },
+    changeBtnChangeName () {
+      this.isChange
+        ? (this.btnChange = 'No Change')
+        : (this.btnChange = 'Change')
+    },
+    changeLoaderState () {
+      this.loader = !this.loader
+    },
+    clearForm () {
+      this.positionSelect = null
+      this.quantyShoes = null
+      this.priceShoes = null
+      this.commision = null
     }
   }
 })
